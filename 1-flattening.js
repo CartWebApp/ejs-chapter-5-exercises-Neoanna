df5633141491ebb186f50f848322c0569302920c
Neoanna <143848719+Neoanna@users.noreply.github.com>
2024-02-14 14:49:06 -0800

    a start

--- 1-flattening.js
+++ 1-flattening.js
@@ -1,5 +1,17 @@
 let arrays = [[1, 2, 3], [4, 5], [6]];
-// Your code here.
+
+arrays.concat(arrays[[2,3]]);
 
 
+function reduce(combine){
+    
+
+
+
+}
+
+
+
+
+console.log(reduce[arrays])
 // → [1, 2, 3, 4, 5, 6]
